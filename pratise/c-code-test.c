014d6c11a8957669f72d668789acc236ffee4c69
LUCKY-SEN2002 <75940443+LUCKY-SEN2002@users.noreply.github.com>
2021-01-08 23:09:12 +0800

    change

--- pratise/c-code-test.c
+++ pratise/c-code-test.c
@@ -68,12 +68,55 @@
 	
 	*/
 //
+//int main() {
+//	char a[] = "abc";//其实该字符窜已经包含\0在里面。
+//	char b[] = { 'a','b','c','\0' };//加入0表示，终止字符串
+//	printf("%s\n", a);
+//	printf("%s\n", b);
+//	return 0;
+//
+//}
+
+ int MAX(int a, int b) {
+	if (a > b) {
+		
+		return a;
+		/*printf("较大值为：%d\n", a);*/
+	}
+	else
+	{
+		return b;
+		/*printf("较大值是：%d\n", b);*/
+	}
+	
+}
+
 int main() {
-	char a[] = "abc";//其实该字符窜已经包含\0在里面。
-	char b[] = { 'a','b','c','\0' };//加入0表示，终止字符串
-	printf("%s\n", a);
-	printf("%s\n", b);
+	int num1=0;
+	int num2=0;
+
+	printf("請輸入兩個數！");
+	scanf("%d%d\n", &num1, &num2);
+	int max = 0;
+	printf("%d%d\n", num1, num2);
+	max=MAX(num1,num2);
+	printf("max為：%d\n", max);
 	return 0;
+
+
+	/*int num1 = 10;
+	int  num2 = 20;
+	if (num1 > num2) {
+		printf("较大值为：%d\n", num1);
+
+	}
+	else
+	{
+		printf("较大值是：%d\n", num2);
+
+	}
+*/
+
 
 }
 
